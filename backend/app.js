ff6db5436ba4dabd63ea72d6b7ce8b2b62aba1bc
Nikhil Wadhwa <101242510+NikhilWadhwa77@users.noreply.github.com>
2024-01-16 21:17:10 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,10 @@ const express = require('express')
 const app = express();
 const router = require("./routes/routes")
 const DB = require('./db/connection')
+const cors = require('cors')
+
+app.use(cors())
+
 app.use(express.json())
 
 DB.connectToMongoDB()
