0bc456285d4da8390d9b64bdc3f31b46fc87b747
Nikhil Wadhwa <101242510+NikhilWadhwa77@users.noreply.github.com>
2024-01-16 21:04:46 +0530

    Update connection.js
    added retryWrites

--- backend/db/connection.js
+++ backend/db/connection.js
@@ -4,7 +4,7 @@ require('dotenv').config()
 const uri = process.env.DB;
 
 exports.connectToMongoDB = () => {
-    mongoose.connect(uri).then(() => {
+    mongoose.connect(uri,{ retryWrites: false}).then(() => {
         console.log("[ SUCCESS ] mongodb connected")
     }).catch((err) => {
         console.log("[ FAILED ] failed to connect mongodb")
